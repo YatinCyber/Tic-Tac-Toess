8b7a623427de845481be7aab19adbb5dc6c50acd
YatinCyber <chabonglatui@gmail.com>
2023-04-18 19:07:06 +0700

    complete

---
+++ src/components/Cell.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState } from 'react';
+import './Cell.scss';
+
+interface CellProps {
+  ind: number;
+  display: string;
+}
+
+const Cell = ({ ind, display, handleCell }) => {
+  const handleClick = () => {
+    handleCell(ind);
+  };
+
+  return (
+    <div className="cell" onClick={handleClick}>
+      <span className={display}></span>
+    </div>
+  );
+};
+
+export default Cell;
